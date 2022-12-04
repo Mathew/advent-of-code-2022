1ae6fdcdaca5c1ce34da94bce7ddb11f2e0cd337
Mathew Taylor <mat@mathewtaylor.co.uk>
2022-12-04 09:53:50 +0000

    Day 4, part 2.

--- cmd/day4/main.go
+++ cmd/day4/main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	intersections := 0
+	fullIntersections := 0
+	partialIntersections := 0
 	for _, pairs := range ASSIGNMENTS {
 
 		assignmentOne := structures.Sequence(pairs[0][0], pairs[0][1])
@@ -16,9 +17,14 @@ func main() {
 
 		if len(intersect) == len(assignmentOne) ||
 			len(intersect) == len(assignmentTwo) {
-			intersections += 1
+			fullIntersections += 1
+		}
+
+		if len(intersect) > 0 {
+			partialIntersections += 1
 		}
 	}
 
-	log.Printf("Part One: %d", intersections)
+	log.Printf("Part One: %d", fullIntersections)
+	log.Printf("Part Two: %d", partialIntersections)
 }
